c628f153acb7c147af6536a83b07bbcd5f1f8dee
Dániel Bordás <bordas.daniel0124@gmail.com>
2026-02-25 09:53:49 +0100

    Add project files.

---
+++ Models/Hozzavalo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReceptAPI.Models;
+
+public partial class Hozzavalo
+{
+    public int Id { get; set; }
+
+    public string Nev { get; set; } = null!;
+
+    public int Kaloria { get; set; }
+}
---
+++ Models/Recept.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReceptAPI.Models;
+
+public partial class Recept
+{
+    public int Id { get; set; }
+
+    public string Nev { get; set; } = null!;
+
+    public int ElkeszitesiIdo { get; set; }
+
+    public int NehezsegId { get; set; }
+
+    public int SzakacsId { get; set; }
+
+    public string Leiras { get; set; } = null!;
+
+    public virtual Nehezseg? Nehezseg { get; set; } = null!;
+
+    public virtual Szakac? Szakacs { get; set; } = null!;
+}
